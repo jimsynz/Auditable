f4efbc3eaa8a06110c939773a8235d70acaee301
James Harton <james@mashd.cc>
2009-12-08 14:00:03 +1300

    Small updates

--- install.rb
+++ install.rb
@@ -8,6 +8,11 @@ rails_dir = File.join(auditable_dir, '..', '..', '..')
 puts "* Generating model and migration..."
 system "#{File.join(rails_dir, 'script', 'generate')} model audit auditable_type:string auditable_id:integer log:text identity:string"
 
+if File.exists? File.join(rails_dir,'app','models','audit.rb')
+  puts "* Backing up existing model.rb"
+  File.move(File.join(rails_dir,'app','models','audit.rb'), File.join(rails_dir,'app','models','audit.rb.old'))
+end
+
 puts "* Making model polymorphic..."
 model = File.open(File.join(rails_dir,'app','models','audit.rb'), 'w')
 model.puts 'class Audit < ActiveRecord::Base'
